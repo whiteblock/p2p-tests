bf265754b24afb376db56aaacb1490536ea56506
Daniel <choidanielw@gmail.com>
2019-05-06 16:28:59 -0700

    fix gitignore + add files

---
+++ gossipsub/utils.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"os"
+	"context"
+	"runtime"
+	"io/ioutil"
+	"path/filepath"
+	libp2p "github.com/libp2p/go-libp2p"
+	p2pd "github.com/libp2p/go-libp2p-daemon"
+	ma "github.com/multiformats/go-multiaddr"
+	c "github.com/libp2p/go-libp2p-daemon/p2pclient"
+)
+
+type makeEndpoints func() (daemon, client ma.Multiaddr, cleanup func(), err error)
+
+func createTempDir() (string, string, func(), error) {
+	root := os.TempDir()
+	dir, err := ioutil.TempDir(root, "p2pd")
+	if err != nil {
+		return "", "", nil, err
+	}
+	daemonPath := filepath.Join(dir, "daemon.sock")
+	clientPath := filepath.Join(dir, "client.sock")
+	closer := func() {
+		os.RemoveAll(dir)
+	}
+	return daemonPath, clientPath, closer, nil
+}
+
+func makeTcpLocalhostEndpoints() (daemon, client ma.Multiaddr, cleanup func(), err error) {
+	daemon, err = ma.NewMultiaddr("/ip4/127.0.0.1/tcp/0")
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	client, err = ma.NewMultiaddr("/ip4/127.0.0.1/tcp/0")
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	cleanup = func() {}
+	return daemon, client, cleanup, nil
+}
+
+func makeUnixEndpoints() (daemon, client ma.Multiaddr, cleanup func(), err error) {
+	daemonPath, clientPath, cleanup, err := createTempDir()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	daemon, _ = ma.NewComponent("unix", daemonPath)
+	client, _ = ma.NewComponent("unix", clientPath)
+	return
+}
+
+func getEndpointsMaker() makeEndpoints {
+	if runtime.GOOS == "windows" {
+		return makeTcpLocalhostEndpoints
+	} else {
+		return makeUnixEndpoints
+	}
+}
+
+func createDaemon(daemonAddr ma.Multiaddr, opts []libp2p.Option) (*p2pd.Daemon, func(), error) {
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	daemon, err := p2pd.NewDaemon(ctx, daemonAddr, "", opts...)
+	if err != nil {
+		return nil, nil, err
+	}
+	err = daemon.EnablePubsub("gossipsub", false, false)
+	if err != nil {
+		return nil, nil, err
+	}
+	return daemon, cancelCtx, nil
+}
+
+func createClient(daemonAddr ma.Multiaddr, clientAddr ma.Multiaddr) (*c.Client, func(), error) {
+	client, err := c.NewClient(daemonAddr, clientAddr)
+	if err != nil {
+		return nil, nil, err
+	}
+	closer := func() {
+		client.Close()
+	}
+	return client, closer, nil
+}
+
+func createDaemonClientPair(opts []libp2p.Option) (*p2pd.Daemon, *c.Client, func(), error) {
+	dmaddr, cmaddr, dirCloser, err := getEndpointsMaker()()
+	daemon, closeDaemon, err := createDaemon(dmaddr, opts)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	client, closeClient, err := createClient(daemon.Listener().Multiaddr(), cmaddr)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	closer := func() {
+		closeDaemon()
+		closeClient()
+		dirCloser()
+	}
+	return daemon, client, closer, nil
+}
