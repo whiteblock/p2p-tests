7b51afc61f17006809934574e5712b8cf9864956
zscole <zak@whiteblock.io>
2019-05-07 18:12:30 -0700

    comments for messages

--- message.go
+++ message.go
@@ -7,10 +7,13 @@ import(
 )
 
 type Message struct {
-	Origin      string            `json:"origin"`
-	Destination string            `json:"destination"`
-	Timestamp   time.Time         `json:"timestamp"`
-	Data        string            `json:"data"`
+	Origin      string            `json:"origin"` //node from which the message was initially sent
+	Destination string            `json:"destination"` //node to which the message is being sent
+	Relayer     string			  `json:"relayer"` //last node which relayed the message
+	Timestamp   time.Time         `json:"timestamp"` //time at which the message was first broadcast
+	Data        string            `json:"data"` //message payload
+	MessageID   string			  `json:"messageid"` //a unique identified associated with a message
+	Nonce       int64             `json:"nonce"` //counter representing order in which message was sent from origin
 }
 
 func (m *Message) Relay() ([]byte, error){
