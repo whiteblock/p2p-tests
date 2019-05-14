81d6ac36a73d5be9566310b519d191c2a51a038d
Nathaniel <vanish172@gmail.com>
2019-05-14 15:07:31 -0700

    automate a few test cases

--- tests/wb-scripps.sh
+++ tests/wb-scripps.sh
@@ -2,13 +2,100 @@
 
 export NO_PRETTY=1
 
-n=20
+#(size,name)
+log(){
+	for ((i=0;i<$1;i++))
+	do
+		wb get log $i > "./${2}/node${i}.log"
+	done
+}
 
-wb build -b libp2p-test -n $n -m 0 -c 0 -y
+wait_for_results(){
+	sleep 2m
+}
 
-sleep 2m
 
-for ((i=0;i<$n;i++))
-do
-    wb get log $i > "/home/daniel/p2ptestdata/n${i}log.txt"
-done
+SENDERS=50
+NODES=100
+SIZE=200
+CONNS=10
+INTERVAL=7812
+
+#SERIES 1 Control
+#a
+wb build -b libp2p-test -n $NODES -m 0 -c 0 -y -o"senders=$SENDERS" -o"payloadSize=$SIZE" -o"connections=$CONNS" -o"interval=$INTERVAL"
+
+wait_for_results
+
+log $NODES series1a
+
+#b
+wb build -b libp2p-test -n $NODES -m 0 -c 0 -y -o"senders=$SENDERS" -o"payloadSize=$SIZE" -o"connections=$CONNS" -o"interval=$INTERVAL"
+
+wait_for_results
+
+log $NODES series1b
+
+#c
+wb build -b libp2p-test -n $NODES -m 0 -c 0 -y -o"senders=$SENDERS" -o"payloadSize=$SIZE" -o"connections=$CONNS" -o"interval=$INTERVAL"
+
+wait_for_results
+
+log $NODES series1c
+
+
+#Series 2: Message Size
+
+SIZE=500
+#a
+wb build -b libp2p-test -n $NODES -m 0 -c 0 -y -o"senders=$SENDERS" -o"payloadSize=$SIZE" -o"connections=$CONNS" -o"interval=$INTERVAL"
+
+wait_for_results
+
+log $NODES series2a
+
+SIZE=500000
+#b
+wb build -b libp2p-test -n $NODES -m 0 -c 0 -y -o"senders=$SENDERS" -o"payloadSize=$SIZE" -o"connections=$CONNS" -o"interval=$INTERVAL"
+
+wait_for_results
+
+log $NODES series2b
+
+SIZE=500000000
+#c
+wb build -b libp2p-test -n $NODES -m 0 -c 0 -y -o"senders=$SENDERS" -o"payloadSize=$SIZE" -o"connections=$CONNS" -o"interval=$INTERVAL"
+
+wait_for_results
+
+log $NODES series2c
+
+#Series 3: Senders
+
+SIZE=200
+NODES=100
+SENDERS=10
+#a
+wb build -b libp2p-test -n $NODES -m 0 -c 0 -y -o"senders=$SENDERS" -o"payloadSize=$SIZE" -o"connections=$CONNS" -o"interval=$INTERVAL"
+
+wait_for_results
+
+log $NODES series3a
+
+SENDERS=40
+
+#b
+wb build -b libp2p-test -n $NODES -m 0 -c 0 -y -o"senders=$SENDERS" -o"payloadSize=$SIZE" -o"connections=$CONNS" -o"interval=$INTERVAL"
+
+wait_for_results
+
+log $NODES series3b
+
+SENDERS=90
+
+#c
+wb build -b libp2p-test -n $NODES -m 0 -c 0 -y -o"senders=$SENDERS" -o"payloadSize=$SIZE" -o"connections=$CONNS" -o"interval=$INTERVAL"
+
+wait_for_results
+
+log $NODES series3c
